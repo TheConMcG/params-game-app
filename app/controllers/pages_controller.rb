39352bd0c76cf83199d1258bdd9a2098cae7e532
Connor <conmmcguire@gmail.com>
2021-10-13 20:10:54 -0500

    fix first_letter downcase issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
   def first_letter
 
     first_letter = params["name"][0]
-    if first_letter.upcase == "A"
+    if first_letter.downcase == "a"
       render json: {message: "Hey your name starts with the first letter of the alphabet"}
     else
       render json: {message: "Your name doesn't start with the first letter of the alphabet, how lame."}
@@ -16,13 +16,12 @@ class PagesController < ApplicationController
   end
 
   def guess_number
-    number = params["number"]
-
-    if number.to_i == 36
+    number = params["number"].to_i
+    if number == 36
       render json: {message: "You got it right!"}
-    elsif number.to_i < 36
+    elsif number < 36
       render json: {message: "Try a little higher!"}
-    elsif number.to_i > 36
+    elsif number > 36
       render json: {message: "Try a little lower!"}
     end
   end
